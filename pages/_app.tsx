a2a66205ff57a34c341a27bffbb0a8f3b47bfd54
talhachaudharyy <chaudharytalhaofficial@gmail.com>
2024-01-18 23:41:59 +0500

    a commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,23 @@
-// pages/_app.js
-
-import Head from 'next/head';
-import '../styles/globals.css'; // Import your global styles here
-import Image from 'next/image';
-function MyApp({ Component, pageProps }) {
-  return (
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+export default function App({ Component, pageProps }: AppProps) {
+  return(
     <>
-      <Head>
-        <title>Talha | Portfolio</title>
-
-
-        <link rel="icon" href="/picofme.png" />
+    <Head>
+      <title>Talha | Portfolio</title>
+      <link rel="icon" href="/picofme.png" />
 
         {/* Add the link to Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
           rel="stylesheet"
         />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
 
-export default MyApp;
+
+    </Head>
+    
+    <Component {...pageProps} />
+    
+    </>)
+}
